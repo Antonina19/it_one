75684caff0f3c0f1dd37e5f81858d0d4311cfe75
tony <tony-94@bk.ru>
2021-08-09 19:05:39 +0300

    Updated Tests for ItOneTests

--- src/test/java/tests/ItOneTests.java
+++ src/test/java/tests/ItOneTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +25,46 @@ public class ItOneTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Переход на страницу 'Услуги'")
+    void checkOpenPageUslugi() {
+        step("Открытие страницы 'https://www.it-one.ru/'", () ->
+                open("https://www.it-one.ru/"));
+
+        step("Клик по кнопке 'Услуги'", () -> {
+            $(".main-intro__slide-button_1").click();
+        });
+
+        step("Свойство title открытой страницы сайта должно быть равно 'Услуги IT_ONE'", () -> {
+            String expectedTitle = "Услуги IT_ONE";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @DisplayName("Переход на страницу 'Карьера'")
+    void checkOpenPageCareer() {
+        step("Открытие страницы 'https://www.it-one.ru/'", () ->
+                open("https://www.it-one.ru/"));
+
+        step("Переключение на третий слайдер", () -> {
+            $(".main-intro__switch_3").click();
+        });
+
+        step("Клик по кнопке 'Карьера'", () -> {
+            $(".main-intro__slide-button_3").click();
+        });
+
+        step("Свойство title открытой страницы сайта должно быть равно 'Карьера в IT_ONE'", () -> {
+            String expectedTitle = "Карьера в IT_ONE";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
     @DisplayName("Поиск заголовка 'Технологии IT_ONE'")
     void checkHeading() {
         step("Открытие страницы 'https://www.it-one.ru/'", () ->
@@ -35,18 +76,22 @@ public class ItOneTests extends TestBase {
     }
 
     @Test
-    @DisplayName("Переход на страницу 'Контакты'")
-    void checkOpenPage() {
+    @DisplayName("Переход на страницу 'Вакансии'")
+    void checkOpenPageVacancies() {
         step("Открытие страницы 'https://www.it-one.ru/'", () ->
                 open("https://www.it-one.ru/"));
 
-        step("Переход на страницу 'Контакты'", () -> {
-            $$(".footer__nav__item").findBy(text("Контакты")).click();
+        step("Клик по кнопке 'Все вакансии'", () -> {
+            $(byId("job-section")).$(".common-button").click();
         });
 
-        step("Проверка заголовка 'Наши контакты'", () -> {
-            $$(".large-line").findBy(text("Наши контакты"));
-        });
+        step("Свойство title открытой страницы сайта должно быть равно 'Вакансии'", () -> {
+            String expectedTitle = "Вакансии";
+            String actualTitle = title();
 
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
     }
+
+
 }
