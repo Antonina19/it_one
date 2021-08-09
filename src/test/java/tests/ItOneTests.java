c87d915edaf0a449be9084a2e8839bc4a32d35ed
tony <tony-94@bk.ru>
2021-08-09 18:22:08 +0300

    Added Tests for ItOneTests

--- src/test/java/tests/ItOneTests.java
+++ src/test/java/tests/ItOneTests.java
@@ -3,8 +3,8 @@ package tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +21,32 @@ public class ItOneTests extends TestBase {
 
             assertThat(actualTitle).isEqualTo(expectedTitle);
         });
+    }
+
+    @Test
+    @DisplayName("Поиск заголовка 'Технологии IT_ONE'")
+    void checkHeading() {
+        step("Открытие страницы 'https://www.it-one.ru/'", () ->
+                open("https://www.it-one.ru/"));
+
+        step("Проверка заголовка 'Технологии IT_ONE'", () -> {
+            $$(".section__header").findBy(text("Технологии IT_ONE"));
+        });
+    }
+
+    @Test
+    @DisplayName("Переход на страницу 'Контакты'")
+    void checkOpenPage() {
+        step("Открытие страницы 'https://www.it-one.ru/'", () ->
+                open("https://www.it-one.ru/"));
+
+        step("Переход на страницу 'Контакты'", () -> {
+            $$(".footer__nav__item").findBy(text("Контакты")).click();
+        });
+
+        step("Проверка заголовка 'Наши контакты'", () -> {
+            $$(".large-line").findBy(text("Наши контакты"));
+        });
+
     }
 }
